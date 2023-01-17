992ee5e71d76f90518a47a8462a28eb6c233b47b
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-01-17 06:01:58 -0800

    Cleaning up

---
+++ test.js
@@ -0,0 +1,25 @@
+import { sleep } from 'k6'
+import http from 'k6/http'
+
+// See https://k6.io/docs/using-k6/options
+export const options = {
+  stages: [
+    { duration: '10s', target: 20 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.02'], // http errors should be less than 2%
+    http_req_duration: ['p(95)<2000'], // 95% requests should be below 2s
+  },
+  ext: {
+    loadimpact: {
+      distribution: {
+        'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 },
+      },
+    },
+  },
+}
+
+export default function main() {
+  let response = http.get('http://127.0.0.1:8080/api/rest/abalance/10')
+  sleep(1)
+}
